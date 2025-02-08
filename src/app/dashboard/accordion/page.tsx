32246716cb40e3bc5c50b736c6a16e743e2a1dda
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-07 18:11:44 -0600

    using carrousel and alerts

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,44 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accesible?",
+    answer: "Yes. it adheres to the WAI-ARIA design pattern",
+  },
+  {
+    id: "item-2",
+    question: "Is it styled?",
+    answer: "Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+  },
+  {
+    id: "item-3",
+    question: "Is it animated?",
+    answer: "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+  {
+    id: "item-4",
+    question: "Item 4",
+    answer: "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+];
 
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type="single" className="w-full">
+        {
+          items.map((item) => (
+            <AccordionItem value={item.id} key={item.id}>
+              <AccordionTrigger>{item.question}</AccordionTrigger>
+              <AccordionContent>
+                {item.answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))
+        }
+        
+      </Accordion>
     </div>
   );
 }
